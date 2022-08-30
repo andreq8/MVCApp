447392eea3b4d59cf10bb889886cf17a8cb50860
André Queirós <andrequeiros86@gmail.com>
2022-08-30 11:26:35 +0100

    add controller

---
+++ MVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "this is the hello world page";
+        }
+        public string Hello()
+        {
+            return "Hello cruel world!";
+        }
+    }
+}
